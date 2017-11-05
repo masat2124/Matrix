ee64df8301a441472a62d69f29edefab71db13bb
pc-user <pc-user@pc-user-PC>
2017-11-06 00:44:50 +0900

    kadai

---
+++ src/Matrix/Matrix_main.java
@@ -0,0 +1,16 @@
+package Matrix;
+
+import java.util.Arrays;
+
+public class Matrix_main {
+
+	public static void main(String[] args) {
+		double a[][] = {{0,1},{2,0},{0,3}};
+		double b[][] = {{1,-1,2},{3,0,4}};
+		
+		Matrix_lib mlib = new Matrix_lib(a,b);
+		System.out.println("Product = "+Arrays.deepToString(mlib.getProduct()));
+		System.out.println("Product = "+Arrays.deepToString(mlib.getTranspose(b)));
+	}
+
+}
