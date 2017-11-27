8257e126129e2619b950e78652950efc40da716c
pc-user <pc-user@pc-user-PC>
2017-11-27 16:15:39 +0900

    kadai1106

--- src/Matrix/Matrix_lib.java
+++ src/Matrix/Matrix_lib.java
@@ -4,65 +4,70 @@ import java.util.Arrays;
 
 public class Matrix_lib {
 
-	public double getlnnerProduct(double a[],double b[]){
-		double answer=0;
-		for(int i = 0;i < a.length;i++) {
-			answer += a[i] * b[i];
-		}
-		return answer;
-	}
-	
-	public double[][] getProduct(double a[][],double b[][]){
-		double answer[][] = new double[a.length][b[0].length];
+	public double [][] getProduct(double a[][], double b[][]){
+		double answer[][] = new  double[a.length][b[0].length];
 		double t[][] = this.getTranspose(b);
-		for(int  i =0;i < a.length;i++) {
-			for(int j = 0;j < b[0].length;j++) {
-				answer[i][j] = this.getlnnerProduct(a[i],t[j]);
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b[0].length; j++) {
+				answer[i][j] = this.getInnerProduct(a[i],t[j]);
 			}
 		}
-		
+			
 		return answer;
+		
 	}
 	
-	public double[][] getTranspose(double a[][]){
+	public double [][] getTranspose(double a[][]){
 		double t[][] = new double[a[0].length][a.length];
-		for(int i = 0;i < a[0].length;i++) {
-			for(int j = 0;j < a.length;j++) {
-				t[i][j] = a[j][i];
+		for (int i = 0; i < a[0].length; i++) {
+			for (int j = 0; j < a.length; j++) {
+				t[i][j]	= a[j][i];
 			}
-		}
+		}	
 		return t;
 	}
-	
+
+	public double getInnerProduct(double a[], double b[]) {
+		
+			double answer = 0;
+			
+			for (int i = 0; i < a.length; i++) {
+				answer += a[i] * b[i];
+			}
+			
+			return answer;
+	}
 	public double getCofactor(double [][] a) {
 		double cofactor = 0;
 		if(a.length == 2) {
-			cofactor = a[0][0] * a[1][1] + a[1][0];
+			cofactor = a[0][0] * a[1][1] - a[0][1] * a[1][0];
 		}
 		else {
-			double tmp[][] = new double[a.length-1][a[0].length-1];
+			double tmp[][] = new double[a.length -1][a[0].length -1];
+		    //1列について展開
 			int index = 0;
-			for(int i = 0; i < a.length;i++) {
-				int p = 0,q = 0;
-				for(int j = 0;j < a.length;j++) {
-					if(i == j)continue;
-					for(int k = 0;k < a[0].length;) {
-						if(index == k)continue; 
+			for(int i = 0; i < a.length; i++ ) {
+				//余因子行列作成
+				int p = 0, q = 0;
+				for(int j = 0; j < a.length; j++) {
+					if( i == j ) continue;
+					for(int k = 0; k < a[0].length; k++) {
+						if( index == k ) continue;
 						tmp[p][q++] = a[j][k];
 					}
 					p++;
 					q = 0;
 				}
-				System.out.println("tmp["+i+"] = "+Arrays.deepToString(tmp));
-				cofactor += a[i][index] * Math.pow(-1,(i + 1) +(index + 1)) * this.getCofactor(tmp);
+				System.out.println("tmp["+i+"] = " + Arrays.deepToString(tmp));
+				cofactor += a[i][index] * Math.pow(-1, ( i + 1 ) + (index + 1 )) * this.getCofactor(tmp);
 			}
-					
 		}
+		
 		return cofactor;
+		
 	}
-	
 	public double [][] getInverse(double[][] a){
-		double answer [][] = new double [a.length][a[0].length];
+	    double answer [][] = new double [a.length][a[0].length];
 	    double tmp[][] = new double[a.length -1][a[0].length -1];
 		double determinant = this.getCofactor(a);
 		
@@ -85,17 +90,15 @@ public class Matrix_lib {
 		answer = this.getTranspose(answer);
 	    return answer;
 	}
-	
 	public double[] getSolution(double a[][], double b[]){
 		double answer[] = new  double[a[0].length];	
 		double c[][] =new double[a.length][a.length];  
 		c = this.getInverse(a);
 		for (int i = 0; i < a.length; i++) {
 			
-				answer[i] = this.getlnnerProduct(c[i],b);
+				answer[i] = this.getInnerProduct(c[i],b);
 			
 		}
 		return answer;
 	}
-
 }
